d6c6d17dbb4e440e7d59d54b54871552a828a74f
MatiasCaceres <kcerescmh98@gmail.com>
2023-08-20 20:34:14 -0300

    fix: navbar mobile

--- public/js/main.js
+++ public/js/main.js
@@ -18,9 +18,9 @@ const abrir = document.querySelector('#abrir');
 const cerrar = document.querySelector('#cerrar');
 
 abrir.addEventListener('click', () =>{
-  nav.classList.add('visible')
+  nav.classList.toggle('visible')
 })
 
-cerrar.addEventListener('click', () =>{
+/* cerrar.addEventListener('click', () =>{
   nav.classList.remove('visible')
-})
+}) */
